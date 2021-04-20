db8afd42517873b4769303ae8f6a69c45780e825
AmoDinho <u14284783@tuks.co.za>
2021-04-20 22:38:41 +0200

    added an aray

--- src/lambda.ts
+++ src/lambda.ts
@@ -3,6 +3,20 @@ import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
 export const handler: APIGatewayProxyHandlerV2 = async (
   event: APIGatewayProxyEventV2
 ) => {
+  const electicCars: Array = [
+    {
+      brand: "Tesla",
+      model: "Model Y",
+    },
+    {
+      brand: "Ford",
+      model: "Mustang",
+    },
+    {
+      brand: "Mercedes",
+      model: "EQS",
+    },
+  ];
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
